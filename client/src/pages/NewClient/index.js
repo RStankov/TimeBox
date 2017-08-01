c8e45d24a1b56459ee5dcb553e96610ac68bac9c
Radoslav Stankov <rstankov@gmail.com>
2017-08-01 12:14:11 +0300

    Form for create

--- client/src/pages/NewClient/index.js
+++ client/src/pages/NewClient/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react';
+import { graphql, gql } from 'react-apollo';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
 
-export default function Page() {
+import { Form, createTheme } from 'components/Form';
+import paths from 'paths';
+
+const theme = createTheme({});
+
+export function Page({ submit, afterSubmit }) {
   return (
     <article>
-      <Header as='h1'>New Client</Header>
+      <Header as="h1">New Client</Header>
+      <Form submit={submit} afterSubmit={afterSubmit} theme={theme}>
+        <Form.Field name="name" />
+        <Form.Submit>Create</Form.Submit>
+      </Form>
     </article>
   );
 }
+
+const MUTATION = gql`
+  mutation CreateClient($input: CreateClientInput!) {
+    response: createClient(input: $input) {
+      node {
+        id
+        name
+      }
+      errors {
+        field
+        messages
+      }
+    }
+  }
+`;
+
+export default connect(null, { push })(graphql(MUTATION, {
+  props: ({ ownProps, mutate }) => ({
+    async submit(input) {
+      const { data: { response } } = await mutate({
+        variables: { input },
+      });
+      return response;
+    },
+    afterSubmit(node) {
+      ownProps.push(paths.client(node));
+    },
+  }),
+})(Page));
