12f8c11458261c96954a8869376e880669fd2b1a
Radoslav Stankov <rstankov@gmail.com>
2017-08-02 22:21:34 +0300

    Always refresh table

--- client/src/pages/Clients/Show/index.js
+++ client/src/pages/Clients/Show/index.js
@@ -33,54 +33,59 @@ export function Page({ data: { loading, client } }) {
         </Menu.Item>
       </Menu>
 
-      <Table celled compact>
-        <Table.Header fullWidth>
-          <Table.Row>
-            <Table.HeaderCell>Date</Table.HeaderCell>
-            <Table.HeaderCell>Period</Table.HeaderCell>
-            <Table.HeaderCell>Billable Time</Table.HeaderCell>
-            <Table.HeaderCell>Description</Table.HeaderCell>
-            <Table.HeaderCell>
-              <Link to={paths.timeLogs.new(client)}>Add</Link>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+      {!client.timeLogs.length &&
+          <center>
+            <Link to={paths.timeLogs.new(client)}>Add the first time log</Link>
+          </center>}
+      {!!client.timeLogs.length &&
+        <Table celled compact>
+          <Table.Header fullWidth>
+            <Table.Row>
+              <Table.HeaderCell>Date</Table.HeaderCell>
+              <Table.HeaderCell>Period</Table.HeaderCell>
+              <Table.HeaderCell>Billable Time</Table.HeaderCell>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+              <Table.HeaderCell>
+                <Link to={paths.timeLogs.new(client)}>Add</Link>
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-        <Table.Body>
-          {client.timeLogs.map(timeLog =>
-            <Table.Row key={timeLog.id}>
-              <Table.Cell>
-                {timeLog.date}
-              </Table.Cell>
-              <Table.Cell>
-                {timeLog.startTime}-{timeLog.endTime}
-              </Table.Cell>
-              <Table.Cell textAlign="right">
-                {timeLog.billableHours}
-              </Table.Cell>
-              <Table.Cell>
-                {timeLog.description}
-              </Table.Cell>
-              <Table.Cell>
-                <Button icon size="small">
-                  <Link to={paths.timeLogs.edit(timeLog)}>
-                    <Icon name="edit" />
-                  </Link>
-                </Button>
-              </Table.Cell>
-            </Table.Row>,
-          )}
-        </Table.Body>
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan="2" />
-            <Table.HeaderCell textAlign="right">
-              {sum} (${sum * client.hourlyRate})
-            </Table.HeaderCell>
-            <Table.HeaderCell colSpan="2" />
-          </Table.Row>
-        </Table.Footer>
-      </Table>
+          <Table.Body>
+            {client.timeLogs.map(timeLog =>
+              <Table.Row key={timeLog.id}>
+                <Table.Cell>
+                  {timeLog.date}
+                </Table.Cell>
+                <Table.Cell>
+                  {timeLog.startTime}-{timeLog.endTime}
+                </Table.Cell>
+                <Table.Cell textAlign="right">
+                  {timeLog.billableHours}
+                </Table.Cell>
+                <Table.Cell>
+                  {timeLog.description}
+                </Table.Cell>
+                <Table.Cell>
+                  <Button icon size="small">
+                    <Link to={paths.timeLogs.edit(timeLog)}>
+                      <Icon name="edit" />
+                    </Link>
+                  </Button>
+                </Table.Cell>
+              </Table.Row>,
+            )}
+          </Table.Body>
+          <Table.Footer>
+            <Table.Row>
+              <Table.HeaderCell colSpan="2" />
+              <Table.HeaderCell textAlign="right">
+                {sum} (${sum * client.hourlyRate})
+              </Table.HeaderCell>
+              <Table.HeaderCell colSpan="2" />
+            </Table.Row>
+          </Table.Footer>
+        </Table>}
     </article>
   );
 }
@@ -104,5 +109,8 @@ const QUERY = gql`
 `;
 
 export default graphql(QUERY, {
-  options: ({ params }) => ({ variables: { id: params.id } }),
+  options: ({ params }) => ({
+    variables: { id: params.id },
+    fetchPolicy: 'network-only',
+  }),
 })(Page);
